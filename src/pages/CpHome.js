58df1722d5cf708bddc9130b46f3346c1ac05bde
ozlemertosun <U2407193@rts-365.dk>
2022-02-08 00:50:00 +0100

    add PhoneInfo component

--- src/pages/CpHome.js
+++ src/pages/CpHome.js
@@ -1,11 +1,13 @@
 import Header from "../assets/components/Header";
 import CpBackground from "../assets/components/CpBackground";
 import Navigation from "../assets/components/Navigation";
+import PhoneInfo from "../assets/components/PhoneInfo";
 
 
 const CpHome = () => {
     return ( 
        <>
+         <PhoneInfo />
         <Header />
         <CpBackground />
         <Navigation />
