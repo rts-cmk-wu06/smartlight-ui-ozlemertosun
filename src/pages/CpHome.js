deca992d34172b835c22c9370b69799a25963cf2
ozlemertosun <U2407193@rts-365.dk>
2022-02-07 22:07:15 +0100

    add CpHome component

---
+++ src/pages/CpHome.js
@@ -0,0 +1,16 @@
+import Header from "../assets/components/Header";
+import CpBackground from "../assets/components/CpBackground";
+import Navigation from "../assets/components/Navigation";
+
+
+const CpHome = () => {
+    return ( 
+       <>
+        <Header />
+        <CpBackground />
+        <Navigation />
+        </> 
+     );
+}
+ 
+export default CpHome;
