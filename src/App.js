9c507f327bcd914dbd33a16f54f25d7a6d64dd01
ozlemertosun <U2407193@rts-365.dk>
2022-02-07 14:28:18 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 
+import Header from './assets/components/Header';
 import './styles/App.css';
 import './styles/index.css'
 
 function App() {
   return (
     <div className="App">
-     
+     <Header />
     </div>
   );
 }
