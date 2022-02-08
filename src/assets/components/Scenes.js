0164904d6691b6f8592d505f6f6e46ed7315418e
ozlemertosun <U2407193@rts-365.dk>
2022-02-08 13:40:57 +0100

    add scenes component

---
+++ src/assets/components/Scenes.js
@@ -0,0 +1,46 @@
+import sceneBulb from "../img/sceneBulb.svg"
+
+const Scenes = () => {
+
+    const SceneArray = [
+        {
+            scene: "Birthday",
+            color: "#FF9B9B"
+        }, 
+       {
+            scene: "Party",
+            color: "#A693EB"
+        }, 
+        {
+            scene: "Relax",
+            color: "#93CAEB"
+        }, 
+        {
+            scene: "Fun", 
+            color: "#89DD94"
+        }, 
+ 
+     ]
+
+    return ( 
+        <>
+
+        <h2 className="SubTitleStyle pt-[29px]">Scenes</h2> 
+      
+        <div className='grid grid-cols-2 gap-x-[25px] gap-y-[19px] mt-[23px]'>  
+            {SceneArray.map((theme) => (
+            <button className={`w-[150px] h-[55px] rounded-[13px] text-white font-bold text-[13px] flex justify-left gap-[23px] pl-[25px] items-center gradient${theme.scene}`}>
+                <div>
+                    <img src={sceneBulb} alt="bubllogo"/>
+                </div>
+                {theme.scene}
+            </button> 
+         ))}  
+        </div>
+        
+      
+    </>
+     );
+}
+ 
+export default Scenes;
