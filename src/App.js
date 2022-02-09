02b70e959214a2748ec19a7644a53e94d6eb9755
ozlemertosun <U2407193@rts-365.dk>
2022-02-09 10:56:08 +0100

    add the following adjustments for development

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './styles/index.css'
 function App() {
   return (
     <div className="App">
-      <motion.div initial={{rotate:360}} animate={{rotate:0}} transition={{duration:40,repeat:Infinity, ease:"linear"}} className='relative top-0 left-0 -z-10'>
+{/*       <motion.div initial={{rotate:360}} animate={{rotate:0}} transition={{duration:40,repeat:Infinity, ease:"linear"}} className='relative top-0 left-0 -z-10'>
         <div className=' absolute h-[194px] w-[194px] rounded-full bg-[#FFFFFF05]'></div>
         <div className=' absolute left-48 top-20 h-[194px] w-[194px] rounded-full bg-[#FFFFFF05]'></div>
         <div className=' absolute top-52 left-4 h-[194px] w-[194px] rounded-full bg-[#FFFFFF05]'></div>
-      </motion.div>
+      </motion.div> */}
         <CpHome />
         <SettingsPage />
     </div>
